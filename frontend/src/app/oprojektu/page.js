3a10cdbeae225c0742f15e721093e378116b63c6
Marijela Miličević <mmilicevi@unipu.hr>
2026-03-23 11:44:49 +0100

    o projektu update

--- frontend/src/app/oprojektu/page.js
+++ frontend/src/app/oprojektu/page.js
@@ -1,35 +1,47 @@
-import HeaderImages from "../components/HeaderImg";
-import MainNav from "../components/MainNav";
-import Footer from "../components/Footer";
 import Link from "next/link";
 
 export default function OProjektuPage() {
   return (
     <main className="bodyoprojektu">
-      <HeaderImages />
-      <MainNav />
-
+      {/* Breadcrumbs ako želiš */}
       <div className="nav-overlay"></div>
       <nav id="breadcrumbs" className="breadcrumbs"></nav>
 
+      {/* Tri okvira / gumbi za podstranice */}
       <section className="tri-okvira">
-        <Link href="/oprojektu/ciljevi" className="okvir">
-          <img src="/assets/images/ciljevi.png" alt="Ciljevi" />
-          <h3>Ciljevi</h3>
+        <Link href="/oprojektu" className="okvir">
+          <img src="/assets/images/ciljevi.png" alt="O projektu" />
+          <h3>O projektu</h3>
         </Link>
 
         <Link href="/oprojektu/rezultati" className="okvir">
-          <img src="/assets/images/rezultati.png" alt="Rezultati" />
-          <h3>Rezultati</h3>
+          <img src="/assets/images/rezultati.png" alt="Rezultati projekta" />
+          <h3>Rezultati projekta</h3>
         </Link>
 
-        <Link href="/oprojektu/tim" className="okvir">
-          <img src="/assets/images/tim.png" alt="Tim" />
-          <h3>Tim</h3>
+        <Link href="/oprojektu/aplikacije" className="okvir">
+          <img src="/assets/images/aplikacije.png" alt="O mrežnoj stranici i aplikacijama" />
+          <h3>O mrežnoj stranici i aplikacijama</h3>
         </Link>
       </section>
 
-      <Footer />
+      {/* Opis projekta */}
+      <section className="opis-projekta">
+        <h2>Dobro došli</h2>
+        <p>
+          Dobro došli na mrežnu stranicu za učenje naglasaka. Ovdje možete razviti tri osnovne kompetencije: 
+          znanje o hrvatskim naglascima, vještinu percepcije naglasaka i intonacije (slušnoga prepoznavanja) 
+          i vještinu produkcije naglasaka (izgovora). Stranica je namijenjena učenju i poučavanju naglasaka 
+          u osnovnoj i srednjoj školi, na fakultetima koji obrazuju studente za javni govor te učenju i 
+          poučavanju hrvatskoga jezika kao stranoga i drugoga jezika. Ona je ujedno i mjesto razvoja 
+          mrežnih jezičnih alata za hrvatski jezik.
+        </p>
+        <p>
+          Aplikacije će se nadopunjavati tijekom trajanja četverogodišnjega projekta. Zasad Vam je dostupna 
+          prva aplikacija: <strong>Što sve znam?</strong> Provjerite svoje znanje o naglascima, odredite 
+          svoj početni naglasni sustav i uživajte u putovanju najizazovnijom normom hrvatskoga standardnog jezika.
+        </p>
+      </section>
     </main>
   );
 }
