088ff5a645a1c240a202f7e0e295b003f0487a87
Mia Rovis <mrovis@student.unipu.hr>
2026-04-08 09:14:21 +0200

    Update
    slike

--- frontend/src/app/components/HeaderImg.js
+++ frontend/src/app/components/HeaderImg.js
@@ -1,10 +1,10 @@
 export default function HeaderImages() {
   return (
     <header className="header-slike">
-      <a href="#"><img src="/assets/images/kisa.png" alt="slika 1" /></a>
-      <a href="#"><img src="/assets/images/sunce.png" alt="slika 2" /></a>
-      <a href="#"><img src="/assets/images/trava.png" alt="slika 3" /></a>
-      <a href="#"><img src="/assets/images/rosa.png" alt="slika 4" /></a>
+      <a href="#"><img src="/assets/images/kisa2.png" alt="slika 1" /></a>
+      <a href="#"><img src="/assets/images/sunce2.png" alt="slika 2" /></a>
+      <a href="#"><img src="/assets/images/trava2.png" alt="slika 3" /></a>
+      <a href="#"><img src="/assets/images/rosa2.png" alt="slika 4" /></a>
     </header>
   )
 }
