edf3fd75103b7a36293bff05e1a5af644f77a99c
Mia Rovis <mrovis@student.unipu.hr>
2026-05-27 09:16:12 +0200

    Distribucijska pravila

--- frontend/src/app/stosveznam/Distribucijska/page.js
+++ frontend/src/app/stosveznam/Distribucijska/page.js
@@ -5,12 +5,14 @@ import Image from "next/image";
 export default function DistribucijskaPravilaPage() {
   return (
     <main className="bodydruga">
-      
 
-      <div className="naslovna">
+      {/* NASLOVNI DIO */}
+      <div className="naslovna-rijec">
         <div className="content-container distribucijska">
-          
-          <h1 className="glavni-naslov">Distribucijska pravila</h1>
+
+          <h1 className="glavni-naslov">
+            Distribucijska pravila
+          </h1>
 
           <div className="slika-distribucijska">
             <Image
@@ -22,40 +24,165 @@ export default function DistribucijskaPravilaPage() {
             />
           </div>
 
+          <p className="uvodna-definicija">
+            <i>
+              Distribucijska pravila određuju raspored naglasaka i
+              prozodema unutar riječi hrvatskoga standardnog jezika.
+            </i>
+          </p>
+
         </div>
       </div>
 
-      <div className="inventar-intro">
+      {/* GLAVNI SADRŽAJ */}
+      <div className="distribucijska-sadrzaj">
 
-      <h2 className="distribucijska-naslov">
-        Tri su osnovna pravila raspodjele ili distribucije prozodema u standardnom jeziku:
-      </h2>
+        {/* UVOD */}
+        <div className="distribucijska-intro">
+          <p>
+            Tri su osnovna pravila raspodjele ili distribucije prozodema u standardnom jeziku:
+          </p>
+        </div>
 
-      <div className="distribucijska-lista">
+        {/* TRI PRAVILA */}
+        <div className="pravila-grid">
 
-        <p className="pravilo">
-          <strong>1.</strong> Jednosložne riječi mogu imati samo silazne naglaske 
-          (pȁs, pȗž).
-        </p>
+          <div className="pravilo-kartica">
+            <span className="pravilo-broj">I.</span>
 
-        <p className="pravilo">
-          <strong>2.</strong> Višesložne riječi mogu imati bilo koji od četiriju naglasaka na prvom slogu 
-          (kȉša, ròsa, sȗnce, tráva), na središnjem slogu dolaze uzlazni naglasci 
-          (slobòda, mandarína), a na zadnjem slogu najčešće nema naglaska. 
-          U određenim slučajevima mogu se javiti dublete: silazni naglasci i na središnjem slogu 
-          (poljoprȉvreda, jugozȃpad) i na zadnjem slogu (vijadȕkt, rokokȏ).
-        </p>
+            <h3>Jednosložne riječi</h3>
 
-        <p className="pravilo">
-          <strong>3.</strong> Dužina se ostvaruje iza naglaska i pripada ili osnovi riječi 
-          (kàpūt, dȍdīr, kȁjkāvka, ȕ grād), ili sufiksima / sufiksalnim morfemima 
-          (mȍmčād, rȁdōst), ili nastavcima / gramatičkim morfemima 
-          (bez sèstrē, sa sèstrōm, bez sestárā, jȃkōga, plȇšēmo).
-        </p>
+            <p>
+              Jednosložne riječi mogu imati samo silazne naglaske (pȁs, pȗž).
+            </p>
+          </div>
+
+          <div className="pravilo-kartica">
+            <span className="pravilo-broj">II.</span>
+
+            <h3>Višesložne riječi</h3>
+
+            <p>
+              Višesložne riječi mogu imati bilo koji od četiriju naglasaka na prvom slogu
+              (kȉša, ròsa, sȗnce, tráva), na središnjem slogu dolaze uzlazni naglasci
+              (slobòda, mandarína), a na zadnjem slogu najčešće nema naglaska.
+              U određenim slučajevima mogu se javiti dublete, silazni naglasci i na
+              središnjem slogu (poljoprȉvreda, jugozȃpad) i na zadnjem slogu
+              (vijadȕkt, rokokȏ).
+            </p>
+          </div>
+
+          <div className="pravilo-kartica">
+            <span className="pravilo-broj">III.</span>
+
+            <h3>Dužina iza naglaska</h3>
+
+            <p>
+              Dužina se ostvaruje iza naglaska i pripada ili osnovi riječi
+              (kàpūt, dȍdīr, kȁjkāvka, ȕ grād), ili sufiksima / sufiksalnim morfemima
+              (mȍmčād, rȁdōst), ili nastavcima / gramatičkim morfemima
+              (bez sèstrē, sa sèstrōm, bez sestárā, jȃkōga, plȇšēmo).
+            </p>
+          </div>
+
+        </div>
+
+        {/* RASPODJELNA PRAVILA */}
+        <div className="distribucijska-blok">
+          <h2>Raspodjelna pravila</h2>
+
+          <p>
+            Uzmemo li u obzir, dakle, polifunkcionalnost standardnoga jezika raspodjelna pravila glase ovako:
+            uzlazni naglasci mogu biti na bilo kojem slogu osim zadnjem, a silazni naglasci mogu biti na svim slogovima riječi.
+          </p>
+
+          <p>
+            Prvo pravilo proizlazi iz činjenice da su za ostvarenje uzlaznih naglasaka potrebna dva sloga,
+            tj. da se visoki ton mora zadržati na zanaglasnom slogu kako bismo ostvarili kratkouzlazni ili
+            dugouzlazni naglasak.
+          </p>
+        </div>
+
+        {/* DRUGO PRAVILO */}
+        <div className="distribucijska-blok">
+          <h2>Silazni naglasci izvan početnoga sloga</h2>
+
+          <p>
+            Drugo pravilo u mnogim se priručnicima navodi restriktivno te se kaže da silazni naglasci mogu biti
+            samo na prvom slogu. Uzus i norma pokazuju da silazni naglasci na nepočetnim slogovima nisu iznimka
+            ili pogreška, već činjenica koja se sustavno pojavljuje u hrvatskom (standardnom) jeziku
+            (u vlastitim imenima, u posuđenicama, u nekim gramatičkim i tvorbenim oblicima) te nema razloga
+            imati pravilo koje opovrgava tu činjenicu.
+          </p>
+
+          <p>
+            U standardnojezičnom izgovoru silazni se naglasci ostvaruju uglavnom na prvom slogu višesložnih riječi
+            (kȉša, jȁbuka, sȗnce, nȃgrada). Pojavljivanje silaznih naglasaka izvan početnoga sloga u neutralnom
+            standardnojezičnom varijetetu najčešće je u (višesložnim) posuđenicama (dijamȁnt, dirigȅnt, dijalȅkt,
+            tornȃdo, akvȃrij), u složenicama (poljoprȉvreda, brodovlȃsnīk), u vlastitim imenima (Marȉja, Voltȇr),
+            u čitanju kratica (HNS – haenȅs, SAD – esadȇ), uzvicima (ijujȕ), u V jd. imenica (Amerikȃnko) i
+            G mn. imenica (podȃtākā, rečȅnīcā, zelȅnīlā) te u govornoj riječi s prednaglasnicom
+            (na sȗncu, u kȕći).
+          </p>
+        </div>
+
+        {/* VARIJETETI */}
+        <div className="distribucijska-blok">
+          <h2>Visoki i niski varijetet</h2>
+
+          <p>
+            U visokom varijetetu, pogledamo li u suvremene priručnike, nije jednoznačno određeno kada je silazni ton
+            izvan početnoga sloga dopušten. U ponekim rječnicima (u Školskom rječniku Instituta za hrvatski jezik i
+            jezikoslovlje ili Velikom rječniku hrvatskoga standardnog jezika Školske knjige) na leksičkoj se razini
+            ne dopušta iznimka od strogo zacrtanih raspodjelnih pravila (poljoprìvreda, brodòvlāsnīk), a u ponekim
+            se gramatikama (npr. u Tvorbi riječi S. Babića) dopušta (poljoprȉvreda, brodovlȃsnīk).
+          </p>
+
+          <p>
+            Ono što je zajedničko svima jest da se iznimke odnose prije svega na govornu riječ
+            (uzimajući u obzir i broj slogova: nȁ vlāk, ali: na vlȁkovima).
+          </p>
+
+          <p>
+            Pojavljivanje silaznoga naglaska na nepočetnom slogu specifično je za niski varijetet
+            (pogledaj, ne vidim i sl.). Takvi naglasni ostvaraji kod glagola još uvijek nisu opisani
+            kao značajka razgovornoga stila standardnoga jezika, no u drugim slučajevima
+            (npr. studȅnt, dramatȕrg) silazni je naglasak izvan početnoga sloga zabilježen kao razgovorni
+            u suvremenim gramatikama jer se javlja i u visinkome naglasnom sustavu koji je u temelju
+            standardnoga jezika (npr. Hrvatska gramatika E. Barić i skupine autora).
+          </p>
+        </div>
+
+        {/* UDARNI NAGLASAK */}
+        <div className="distribucijska-blok">
+          <h2>Udarni naglasak i akut</h2>
+
+          <p>
+            Govornici koji često ostvaruju silazne naglaske na nepočetnom slogu, osobito u glagolima,
+            najčešće su govornici koji govore udarnim naglasnim sustavom pa se svi njihovi naglasci
+            mogu tumačiti kao udarni naglasci.
+          </p>
+
+          <p>
+            Budući da je najsličniji kratkosilaznome, udarni naglasak često i bilježimo jednako kao
+            kratkosilazni, ali govornici iz udarnoga sustava na mjestu dugouzlaznoga naglaska ponekad
+            ostvaruju i dugi naglašeni slog koji je teško interpretirati.
+          </p>
+
+          <p>
+            Percipira se kao udarni, kao udarni dugi ili kao ravan, a ponekad se, zbog očekivanja
+            dugouzlaznoga, i percipira kao dugouzlazni.
+          </p>
+
+          <div className="napomena-akut">
+            <strong>Akut</strong> je specifičan uzlazni naglasak koji se pojavljuje u dijalektima
+            (i nije dio standardnoga jezika) i koji se može ostvariti na jednom slogu
+            (jã, mũl, žãl) pa i na posljednjem slogu riječi
+            (vapõr, bidũn, papĩr).
+          </div>
+        </div>
 
       </div>
-
-    </div>
 
     </main>
   );
