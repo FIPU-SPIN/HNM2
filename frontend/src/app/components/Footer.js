1676e1bb1e9077ae1843b329825e58cdcc7e9fa5
Marijela Miličević <mmilicevi@unipu.hr>
2026-06-01 16:38:03 +0200

    uređivanje poveznica i slika

--- frontend/src/app/components/Footer.js
+++ frontend/src/app/components/Footer.js
@@ -5,13 +5,17 @@ export default function Footer() {
         <div className="footerlijevo">
           <img src="/assets/images/knjiga1.png" alt="KSTR" />
           <img src="/assets/images/knjiga2.png" alt="KSTR" />
-          <img src="/assets/images/eunext.jpg" alt="EU" className="footer-logo" />
-          <img src="/assets/images/ffpulogo.png" alt="FFPU" className="footer-logo" />
+          <a href="https://ec.europa.eu/regional_policy/home_en" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/images/eunext.jpg" alt="EU" className="footer-logo" />
+          </a>
+          <a href="https://ffpu.unipu.hr/" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/images/ffpulogo.png" alt="FFPU" className="footer-logo" />
+          </a>
         </div>
 
         <div className="footerdesno">
-          <a href="#">poveznica1</a>
-          <a href="#">poveznica2</a>
+          <a href="#">Poveznica 1</a>
+          <a href="#">Poveznica 2</a>
         </div>
       </div>
 
