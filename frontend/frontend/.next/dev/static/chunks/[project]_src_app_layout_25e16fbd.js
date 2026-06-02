fe88d7c9b5029e7e0c3be968a89a458147c7ec23
Mia Rovis <mrovis@student.unipu.hr>
2026-06-02 08:51:51 +0200

    Uklanjanje duplikata

--- frontend/frontend/.next/dev/static/chunks/[project]_src_app_layout_25e16fbd.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[project]_src_app_style_3ec1de61.css",
-  "static/chunks/[root-of-the-server]__ab426221._.js"
-],
-    source: "dynamic"
-});
