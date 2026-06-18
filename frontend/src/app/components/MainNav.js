72bf64f07f36e820967b559ced79e0fee7dde4c0
Mia Rovis <mrovis@student.unipu.hr>
2026-06-18 10:04:45 +0200

    Burger fix

--- frontend/src/app/components/MainNav.js
+++ frontend/src/app/components/MainNav.js
@@ -111,7 +111,6 @@ export default function MainNav() {
 
             <ul className={`dropdown-menu ${openDropdown === "stosveznam" ? "open" : ""}`}>
               <li><Link href="/stosveznam/GovornaRijec">Govorna riječ</Link></li>
-              
               <li><Link href="/stosveznam/Inventar">Inventar</Link></li>
               <li><Link href="/stosveznam/Distribucijska">Distribucijska pravila</Link></li>
               <li><Link href="/stosveznam/NaglasciUParadigmi">Naglasci u paradigmi</Link></li>
